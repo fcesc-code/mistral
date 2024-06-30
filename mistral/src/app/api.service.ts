6699b7ca7418bb0786cefda3ec9184ce21a2ed52
fcesc-code <bpfran@gmail.com>
2024-06-30 02:41:35 +0200

    working front

--- mistral/src/app/api.service.ts
+++ mistral/src/app/api.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import MistralClient from '@mistralai/mistralai';
 
 @Injectable({
   providedIn: 'root',
@@ -10,21 +9,25 @@ export class ApiService {
 
   ENDPOINT = 'http://localhost:3000/api';
 
+  // async get_test_response() {
+  //   const response = await fetch(this.ENDPOINT, {
+  //     method: 'GET',
+  //   });
+  //   return response.json();
+  // }
+
   async get_test_response() {
-    const response = await fetch(this.ENDPOINT, {
-      method: 'GET',
-    });
-    return response.json();
+    return this.http.get(this.ENDPOINT);
   }
 
-  async get_chat_response(prompt: string) {
-    console.log('INFO', prompt);
-    const response = await fetch(this.ENDPOINT, {
-      method: 'POST',
-      body: prompt,
-    });
-    return response.json();
-  }
+  // async get_chat_response(prompt: string) {
+  //   console.log('INFO', prompt);
+  //   const response = await fetch(this.ENDPOINT, {
+  //     method: 'POST',
+  //     body: prompt,
+  //   });
+  //   return response.json();
+  // }
 
   get_chat_response_2(prompt: string) {
     return this.http.post(this.ENDPOINT, { prompt: prompt });
--- mistral/src/app/prompt/prompt.component.ts
+++ mistral/src/app/prompt/prompt.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../api.service';
 import { inject } from '@angular/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
-import { map } from 'rxjs/operators';
+// import { MatFormFieldModule } from '@angular/material/form-field';
+// import { MatInputModule } from '@angular/material/input';
+// import { FormsModule } from '@angular/forms';
+// import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-prompt',
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule],
+  // imports: [FormsModule, MatFormFieldModule],
+  imports: [],
   templateUrl: './prompt.component.html',
   styleUrl: './prompt.component.sass',
 })
@@ -49,9 +50,10 @@ export class PromptComponent {
     if (event.target.value) {
       this.prompt = event.target.value;
       console.log('user prompt: ', this.prompt);
-      this.apiService
-        .get_chat_response_2(this.prompt)
-        .subscribe((data: any) => (this.prompt_response = data));
+      this.apiService.get_chat_response_2(this.prompt).subscribe({
+        next: (data: any) => (this.prompt_response = data),
+        error: (error: any) => console.error('mistral error:', error),
+      });
       console.log('mistral response:', this.prompt_response);
     }
   }
